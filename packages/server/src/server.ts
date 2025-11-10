1ef4cba3ec52c0160dd766efabd66189ba42a665
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2025-11-10 16:02:25 +0530

    chore: meged db and server module and added authentication library

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import {auth} from "./lib/auth";
+import { toNodeHandler } from "better-auth/node";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello from webGPT backend");
 });
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 const port = process.env.PORT || 4000;
 
